244c1c4996363848410181dac1c55d99a76c7132
dessip77 <81189109+dessip77@users.noreply.github.com>
2021-03-27 19:08:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ let { spawnSync } = require('child_process')
 let WAConnection = simple.WAConnection(_WAConnection)
 
 
-global.owner = ['556696159143@s.whatsapp.net'] // Put your number here
-global.mods = ['556696159143@s.whatsapp.net'] // Want some help?
-global.prems = ['556696159143@s.whatsapp.net'] // Premium user has unlimited limit
+global.owner = ['5511985598458@s.whatsapp.net'] // Put your number here
+global.mods = ['5511985598458@s.whatsapp.net'] // Want some help?
+global.prems = ['5511985598458@s.whatsapp.net'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://deiltonp.com',
